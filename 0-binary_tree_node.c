e9481c13c4bdcc8ee2ccdf0b9d200af20cf0bffc
Jedidah <jedidahwambuguw@gmail.com>
2023-03-28 23:48:46 +0300

    Added 0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,10 +13,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	
+
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->n = value;
